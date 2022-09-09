d99dd388f8c2d657196c1601f8a170480d7e123f
robinsonmourao <49078615+robinsonmourao@users.noreply.github.com>
2022-09-09 09:53:32 -0300

    Initial commit

---
+++ src/main/java/com/loja/lojavirtualAPI/LojavirtualApiApplication.java
@@ -0,0 +1,13 @@
+package com.loja.lojavirtualAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loja/lojavirtualAPI/LojavirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.lojavirtualAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
